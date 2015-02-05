4f8044c1bc8aa34233cb32f33fcfdc3eaa122ede
drakin <Kobyshev.sergey@gmail.com>
2015-02-05 16:11:01 +0300

    Test Vector Refactoring
    add Travis CI

--- src/test/java/TestVector.java
+++ src/test/java/TestVector.java
@@ -9,27 +9,24 @@ import static org.junit.Assert.assertNotNull;
  */
 public class TestVector {
     Vector currentVector,add1Vector,add2Vector,sumVector;
-    public static final int DIMENSION_ADD1 = 2;
     public static final float[] VALUES_ADD1 = new float[] {1.f, 2.f};
-    public static final int DIMENSION_ADD2 = 2;
     public static final float[] VALUES_ADD2 = new float[] {3.f, 4.f};
-    public static final int DIMENSION_SUM = 2;
     public static final float[] VALUES_SUM = new float[]{VALUES_ADD1[0]+VALUES_ADD2[0],VALUES_ADD1[1]+VALUES_ADD2[1]};
     @Before
     public void setUp(){
-        currentVector = new Vector(DIMENSION_ADD1);
+        currentVector = new Vector(VALUES_ADD1.length);
         currentVector.coordinates = VALUES_ADD1;
-        add1Vector = new Vector(DIMENSION_ADD1);
+        add1Vector = new Vector(VALUES_ADD1.length);
         add1Vector.coordinates = VALUES_ADD1;
-        add2Vector = new Vector(DIMENSION_ADD2);
+        add2Vector = new Vector(VALUES_ADD2.length);
         add2Vector.coordinates = VALUES_ADD2;
-        sumVector = new Vector(DIMENSION_SUM);
+        sumVector = new Vector(VALUES_SUM.length);
         sumVector.coordinates = VALUES_SUM;
     }
     @Test
     public void testVectorConstructor() {
         assertNotNull("Vector не был создан", currentVector);
-        assertEquals("Размерности не совпадают", DIMENSION_ADD1, currentVector.coordinates.length);
+        assertEquals("Размерности не совпадают", VALUES_ADD1.length, currentVector.coordinates.length);
     }
     @Test
     public void testEquals(){
